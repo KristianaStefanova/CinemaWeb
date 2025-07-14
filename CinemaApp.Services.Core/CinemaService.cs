d9cd390387e6910ed0e68635713e9afb05522768
kristiana95 <christiana.stefanova@gmail.com>
2025-07-14 20:09:02 +0200

    Introduce Cinema repository, service and controller.Implement Index functionality

---
+++ CinemaApp.Services.Core/CinemaService.cs
@@ -0,0 +1,35 @@
+﻿using CinemaApp.Data.Repository.Interfaces;
+using CinemaApp.Services.Core.Interfaces;
+using CinemaApp.Web.ViewModels.Cinema;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApp.Services.Core
+{
+    public class CinemaService : ICinemaService
+    {
+        private readonly ICinemaRepository cinemaRepository;
+        public CinemaService(ICinemaRepository cinemaRepository)
+        {
+            this.cinemaRepository = cinemaRepository;
+        }
+        public async Task<IEnumerable<UsersCinemaIndexViewModel>> GetAllCinemasUserViewAsync()
+        {
+            IEnumerable<UsersCinemaIndexViewModel> allCinemasUsersView = await this.cinemaRepository
+                .GetAllAttached()
+                .Select(c => new UsersCinemaIndexViewModel()
+                {
+                    Id = c.Id.ToString(),
+                    Name = c.Name,
+                    Location = c.Location
+                })
+                .ToArrayAsync();
+
+            return allCinemasUsersView;
+        }
+    }
+}
---
+++ CinemaApp/Controllers/CinemaController.cs
@@ -0,0 +1,32 @@
+﻿using CinemaApp.Services.Core.Interfaces;
+using CinemaApp.Web.ViewModels.Cinema;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaApp.Web.Controllers
+{
+    public class CinemaController : BaseController
+    {
+        private readonly ICinemaService cinemaService;
+
+        public CinemaController(ICinemaService cinemaService)
+        {
+            this.cinemaService = cinemaService;
+        }
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<UsersCinemaIndexViewModel> allCinemasUserView = await this.cinemaService
+                 .GetAllCinemasUserViewAsync();
+
+            return View(allCinemasUserView);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Program(string? id)
+        {
+            
+        }
+    }
+}
