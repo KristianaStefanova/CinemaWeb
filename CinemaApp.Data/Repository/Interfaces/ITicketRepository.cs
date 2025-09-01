ccfb9e10364d3bb1e9924b73ac56a12453d9f09e
kristiana95 <christiana.stefanova@gmail.com>
2025-09-01 12:54:46 +0200

    Introduce WebApi for dynamic app functionalities.

---
+++ CinemaApp.Data/Repository/Interfaces/ITicketRepository.cs
@@ -0,0 +1,14 @@
+﻿using CinemaApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaApp.Data.Repository.Interfaces
+{
+    internal interface ITicketRepository
+        : IRepository<Ticket, Guid>, IAsyncRepository<Ticket, Guid>
+    {
+    }
+}
